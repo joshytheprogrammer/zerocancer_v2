53b46a01f382931aaf08522c20f767da53fec50b
samuelcodess <sifenkwe@stu.cu.edu.ng>
2024-02-10 08:19:32 +0100

    made changes

--- script.js
+++ script.js
@@ -34,3 +34,16 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('imageContainer').appendChild(additionalImage);
     });
 });
+
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('toggle_btn i')
+const dropDownMenu = document.querySelector('dropdown_menu')
+
+toggleBtn.onclick = function (){
+    dropDownMenu.classList.toggle('open')
+    const isopen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isopen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
