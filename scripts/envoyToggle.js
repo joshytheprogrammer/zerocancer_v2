9732a17239180f03df540bf440d18064ec636839
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-02-29 17:57:42 +0100

    made changes: ''

--- scripts/envoyToggle.js
+++ scripts/envoyToggle.js
@@ -102,28 +102,51 @@ const findEnvoyById = (id) => {
 
 const populateSlideMenu = (envoy) => {
   const profileImage = slideMenu.querySelector(".body img");
-  const name = slideMenu.querySelector(".fi_ .key"); // Updated selector
-  const title = slideMenu.querySelector(".fi_ .value"); // Updated selector
-  const placeOfWork = slideMenu.querySelector(".se_ .value"); // Updated selector
-  const visionLink = slideMenu.querySelector(".th_ .value a"); // Updated selector
+  const name = slideMenu.querySelector(".fi_ .key");
+  const title = slideMenu.querySelector(".fi_ .value");
+  const placeOfWork = slideMenu.querySelector(".se_ .value");
+  const visionLink = slideMenu.querySelector(".th_ .value a");
+  const visionStatement = slideMenu.querySelector(".fif_ .value");
   const specialtiesContainer = slideMenu.querySelector(".fo_ .value.s");
+  const membershipsContainer = slideMenu.querySelector(".six_ .value.m");
+  const qualificationsContainer = slideMenu.querySelector(".sev_ .value.q");
 
   // Add other elements you want to populate in the slide menu
   profileImage.src = envoy.profileImage;
-  profileImage.alt = 'profile for '+envoy.fullName+' in zero cancer envoy list.'
+  profileImage.alt = 'profile for ' + envoy.fullName + ' in zero cancer envoy list.';
   name.textContent = envoy.fullName;
   title.textContent = envoy.title;
   placeOfWork.textContent = envoy.placeOfWork;
+  visionStatement.textContent = envoy.visionStatement;
   visionLink.href = envoy.visionLink;
   visionLink.textContent = envoy.fullName.toLowerCase().replace(/ /g, "_") + "_vision_statement";
 
-  specialtiesContainer.textContent = ""
-
+  // Populate Specialties
+  specialtiesContainer.textContent = "";
   envoy.specialties.forEach((specialty) => {
     const specialtyElement = document.createElement("div");
     specialtyElement.classList.add("t");
     specialtyElement.textContent = specialty.label;
     specialtiesContainer.appendChild(specialtyElement);
   });
+
+  // Populate Memberships
+  membershipsContainer.textContent = "";
+  envoy.memberships.forEach((membership) => {
+    const membershipElement = document.createElement("div");
+    membershipElement.classList.add("t");
+    membershipElement.textContent = membership.label;
+    membershipsContainer.appendChild(membershipElement);
+  });
+
+  // Populate Qualifications
+  qualificationsContainer.textContent = "";
+  envoy.qualifications.forEach((qualification) => {
+    const qualificationElement = document.createElement("div");
+    qualificationElement.classList.add("t");
+    qualificationElement.textContent = qualification.label;
+    qualificationsContainer.appendChild(qualificationElement);
+  });
 };
+
 
