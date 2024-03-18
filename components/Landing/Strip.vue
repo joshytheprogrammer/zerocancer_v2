f2056f10b80b359c2feaab2a36d60b1525cc2166
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-03-18 14:35:49 +0100

    made changes: ''

---
+++ components/Landing/Strip.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="strip">
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    <LandingStrip />
+  </div>
+</template>
+
+<script setup>
+useSeoMeta({
+  title: 'Zerocancer: Preventing and Managing Cancer Effectively',
+  ogTitle: 'Zerocancer: Preventing and Managing Cancer Effectively',
+  description: 'Zerocancer is dedicated to minimizing new cases of cervical, breast, prostate, and colon cancers through extensive awareness campaigns, screening initiatives, and access to affordable care. Our mission extends to supporting diagnosed cases and connecting individuals with high-risk screenings and early interventions.',
+  ogDescription: 'Zerocancer is dedicated to minimizing new cases of cervical, breast, prostate, and colon cancers through extensive awareness campaigns, screening initiatives, and access to affordable care. Our mission extends to supporting diagnosed cases and connecting individuals with high-risk screenings and early interventions.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+  ogType: 'website',
+  ogUrl: 'https://zerocancer.africa',
+  ogSiteName: 'Zerocancer'
+});
+</script>
+
