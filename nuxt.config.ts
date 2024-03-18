5b0be2f047f2e75a8d22242277df7c1d177508a5
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-03-18 14:25:08 +0100

    made changes: ''

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    pageTransition: { name: 'fade', mode: 'out-in' }
+  },
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt']
 })
+ 
