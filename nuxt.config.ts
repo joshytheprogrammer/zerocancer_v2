19e9c224fc413be4d833fc808cecfd2e81757282
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-03-19 10:27:36 +0100

    made changes: ''

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,19 @@ export default defineNuxtConfig({
     pageTransition: { name: 'fade', mode: 'out-in' }
   },
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt']
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt', 'nuxt-vuefire'],
+  vuefire: {
+    auth: {
+      enabled: false
+    },
+    config: {
+      apiKey: "AIzaSyAYJ5T1fu_nY_OzFDyHvbrmU67I0NzNCzI",
+      authDomain: "zerocancerafrica.firebaseapp.com",
+      projectId: "zerocancerafrica",
+      storageBucket: "zerocancerafrica.appspot.com",
+      messagingSenderId: "503796621829",
+      appId: "1:503796621829:web:a2a4a5ee13bff39e94f26c"
+    },
+  },
 })
  
