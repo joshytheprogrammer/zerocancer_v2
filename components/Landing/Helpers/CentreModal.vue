1010b4aa0b3f9cee1b8d944737c1a2fd1606331d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-03-19 14:16:55 +0100

    made changes: 'prepping for modal'

---
+++ components/Landing/Helpers/CentreModal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <GlobalHelpersModal>
+
+    </GlobalHelpersModal>
+  </div>
+</template>
+
+<script setup>
+// Define props to accept centres
+</script>
+
+<style lang="scss" scoped>
+
+</style>
