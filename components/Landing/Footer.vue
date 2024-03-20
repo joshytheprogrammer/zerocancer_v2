1e982165891b422da1814f29596d5e4f719fabb5
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-03-20 19:58:59 +0100

    made changes: ''

--- components/Landing/Footer.vue
+++ components/Landing/Footer.vue
@@ -1,49 +1,50 @@
 <template>
-  <footer>
-    <div class="container">
-      <img src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1707606121/zerocancer/zero_cancer-FULL-LOGO_bfny3v.svg" alt="Zero cancer logo">
-      <div class="content">
-        <div class="f">
-          <p>Reach out to us to discover how we can help you <br> with issues relating to cancer.</p>
-          <p><a href="mailto:admin@zerocancer.africa">admin@zerocancer.africa</a></p>
-        </div>
-        <div class="s">
-          <div class="company">
-            <h2>Company</h2>
-            <div class="links">
-              <a href="#about">About</a>
-              <a href="#explorer">Explorer</a>
-              <a href="#dvss">Mobilab DVSS</a>
-              <a href="#blog">Blog</a>
+  <footer class="bg-primary-950 text-white mt-20 py-20">
+    <div class=" w-full lg:max-w-[80%] mx-auto px-4 text-white">
+        <img src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1707606121/zerocancer/zero_cancer-FULL-LOGO_bfny3v.svg" alt="Zero cancer logo" class="w-auto">
+        <div class="content flex flex-col lg:flex-row justify-between">
+            <div class="f">
+                <p class="my-8">Reach out to us to discover how we can help you <br> with issues relating to cancer.</p>
+                <p><a href="mailto:admin@zerocancer.africa" class="text-white text-3xl">admin@zerocancer.africa</a></p>
             </div>
-          </div>
-          <div class="_connect">
-            <h2>Connect</h2>
-            <div class="links">
-              <a href="#">Facebook</a>
-              <a href="#">Twitter</a>
-              <a href="https://www.linkedin.com/company/zerocancer/">Linkedin</a>
-              <a href="#">Instagram</a>
+            <div class="s flex justify-between lg:gap-16 my-8 lg:my-0">
+                <div class="company">
+                    <h2 class="text-2xl lg:text-3xl font-bold pb-6">Company</h2>
+                    <div class="flex flex-col space-y-6">
+                        <a href="#about" class="text-white">About</a>
+                        <a href="#explorer" class="text-white">Explorer</a>
+                        <a href="#dvss" class="text-white">Mobilab DVSS</a>
+                        <a href="#blog" class="text-white">Blog</a>
+                    </div>
+                </div>
+                <div class="_connect">
+                    <h2 class="text-2xl lg:text-3xl font-bold pb-6">Connect</h2>
+                    <div class="flex flex-col space-y-6">
+                        <a href="#" class="text-white">Facebook</a>
+                        <a href="#" class="text-white">Twitter</a>
+                        <a href="https://www.linkedin.com/company/zerocancer/" class="text-white">Linkedin</a>
+                        <a href="#" class="text-white">Instagram</a>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <hr>
-      <div class="final">
-        <p>&copy; <span id="currentYear"></span> ZeroCancer. All rights reserved.</p>
-        <div>
-          <p>Legal Notice</p>
-          <p>Terms & Conditions</p>
+        <hr class="my-8">
+        <div class="final flex justify-between text-base">
+            <p>&copy; <span id="currentYear"></span> ZeroCancer. All rights reserved.</p>
+            <div class="flex gap-8 font-semibold">
+                <p>Legal Notice</p>
+                <p>Terms & Conditions</p>
+            </div>
         </div>
-      </div>
     </div>
-  </footer>
+</footer>
+
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="postcss" scoped>
 
 </style>
