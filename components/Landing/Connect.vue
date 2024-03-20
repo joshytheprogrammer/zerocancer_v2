3e74b3cea7b07bfd427b2efd56b65fa96493c1bb
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-03-20 19:38:16 +0100

    made changes: ''

---
+++ components/Landing/Connect.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="block w-full mx-4 my-0 px-4 bg-primary-100 text-primary-950 lg:max-w-[80%] lg:grid grid-cols-[1fr_1fr] items-center justify-items-end lg:mx-auto lg:my-20 rounded-xl ">
+    <div class="px-0 py-8 lg:flex flex-col gap-4 lg:px-12">
+      <h2 class=" text-3xl">Connect with top tier healthcare professionals</h2>
+      <p class=" text-base leading-8 px-0 py-4">Stay tuned for a smarter, more convenient way to care for yourself with our innovative Digital App. Get notified when it's available for download.</p>
+      <div class="block w-full lg:flex flex-row justify-start items-center text-base gap-8">
+        <input class=" w-full lg:w-[300px] h-[60px] border border-primary-600 text-inherit px-4 py-0 rounded-md border-solid placeholder:text-primary-600 focus:outline-none" type="email" disabled placeholder="Enter your email address ">
+        <button class=" w-full lg:w-fit mx-0 my-4 rounded-md text-inherit bg-primary-600 text-white h-[60px] cursor-pointer px-8 py-4  border-none" type="button" disabled>Submit</button>  
+      </div>
+    </div>
+    <img class="hidden lg:block w-full max-w-[600px]" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1707574931/zerocancer/mobile_app_q7phoq.png" alt="c">
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
---
+++ components/Landing/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <footer>
+    <div class="container">
+      <img src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1707606121/zerocancer/zero_cancer-FULL-LOGO_bfny3v.svg" alt="Zero cancer logo">
+      <div class="content">
+        <div class="f">
+          <p>Reach out to us to discover how we can help you <br> with issues relating to cancer.</p>
+          <p><a href="mailto:admin@zerocancer.africa">admin@zerocancer.africa</a></p>
+        </div>
+        <div class="s">
+          <div class="company">
+            <h2>Company</h2>
+            <div class="links">
+              <a href="#about">About</a>
+              <a href="#explorer">Explorer</a>
+              <a href="#dvss">Mobilab DVSS</a>
+              <a href="#blog">Blog</a>
+            </div>
+          </div>
+          <div class="_connect">
+            <h2>Connect</h2>
+            <div class="links">
+              <a href="#">Facebook</a>
+              <a href="#">Twitter</a>
+              <a href="https://www.linkedin.com/company/zerocancer/">Linkedin</a>
+              <a href="#">Instagram</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <div class="final">
+        <p>&copy; <span id="currentYear"></span> ZeroCancer. All rights reserved.</p>
+        <div>
+          <p>Legal Notice</p>
+          <p>Terms & Conditions</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
