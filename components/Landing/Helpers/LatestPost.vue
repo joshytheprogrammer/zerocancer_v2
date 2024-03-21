0453eb5d5b4b55be93b306df8a652cecd2b8f614
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-03-21 17:22:21 +0100

    optimizing for mobile

--- components/Landing/Helpers/LatestPost.vue
+++ components/Landing/Helpers/LatestPost.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full flex flex-col-reverse lg:grid grid-cols-[1fr_1fr] items-center justify-items-end bg-[rgba(145,145,145,0.1)] mx-0 my-16 rounded-xl">
     <div class="p-4 lg:p-8">
-      <span class=" block w-fit border border-primary-800 bg-primary-800 text-white text-xs uppercase mb-8 px-4 py-2 rounded-full border-solid">featured</span>
-      <h2 class="text-3xl">Empowering Lives: A Guide to Early Cancer Detection</h2>
-      <p class=" text-baseleading-5 md:leading-8 px-0 py-4"> In this post, explore the vital role of early cancer detection with a guide on screening methods, risk factors, and proactive measures. Discover the latest diagnostic technologies and insights from survivor stories, emphasizing the impact of early intervention. Join us in empowering lives through awareness and education for a world with zero cancer. #EarlyDetection #CancerPrevention #EmpowerLives</p>
+      <span class="block w-fit border border-primary-800 bg-primary-800 text-white text-[8px] lg:text-xs uppercase mb-8 px-4 py-2 rounded-full border-solid">featured</span>
+      <h2 class="text-xl lg:text-2xl ">Empowering Lives: A Guide to Early Cancer Detection</h2>
+      <p class="text-xs lg:text-base leading-5 md:leading-8 px-0 py-4"> In this post, explore the vital role of early cancer detection with a guide on screening methods, risk factors, and proactive measures. Discover the latest diagnostic technologies and insights from survivor stories, emphasizing the impact of early intervention. Join us in empowering lives through awareness and education for a world with zero cancer. #EarlyDetection #CancerPrevention #EmpowerLives</p>
       <button class=" bg-primary-600 cursor-pointer text-white text-base font-medium px-12 py-4 rounded-md border-none;" disabled>Read</button>
     </div>
     <img class=" w-full h-full object-cover rounded-tr-md rounded-br-md" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1707574959/zerocancer/2df6c2afb0790b632e24ce69d1446a4f_nep50u.png" alt="c">
