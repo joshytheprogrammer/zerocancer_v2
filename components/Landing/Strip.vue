1a21220f0cc4fbacdf2cf9cb4cc4a1270510c2d1
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-03-21 16:24:06 +0100

    optimizing for mobile

--- components/Landing/Strip.vue
+++ components/Landing/Strip.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-primary-700 w-full text-center text-white text-xs py-1">
+  <div class="bg-primary-700 w-full text-center text-white text-[8px] text-xs p-1">
     <p>Welcome to the updated version of the zerocancer website. You can revert to the old one <a class="underline text-secondary-400" href="https://v1.zerocancer.africa">here</a></p>
   </div>
 </template>
