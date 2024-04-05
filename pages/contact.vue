ebce376b1b686188147f517f7d1be977d1c5e5da
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-04-05 14:49:49 +0100

    made changes: ''

---
+++ pages/contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="h-screen">
+    <BlogHelpersNav />
+    <BlogHelpersMNav />
+    <div class="h-screen">
+      <iframe src="https://us22.list-manage.com/contact-form?u=b09db889dd4df3dc722a5b413&amp;form_id=f9205778621f103c69628663aa863080" frameborder="0" scrolling="no" style="width:100%; height:100%;"></iframe>
+    </div>
+    <LandingFooter />
+  </div>
+</template>
+
+<script>
+// Your Vue script setup/logic
+</script>
+
+<style scoped>
+/* Your component-specific styles */
+</style>
