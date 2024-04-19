29e37aaffec5cc3b33fc079745c1465d9c30afe1
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-04-19 16:55:35 +0100

    made changes: ''

---
+++ components/Donate/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="bg-primary-950 text-white text-center py-8">
+    <p class="py-4 lg:py-0">&copy; <span class=" lg:font-bold lg:text-lg">{{ new Date().getFullYear() }}</span> ZeroCancer. All rights reserved.</p>
+  </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/Donate/Jumbotron.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="border-b h-fit bg-cover bg-center bg-no-repeat ">
+    <div class="flex justify-start align-middle items-start flex-col lg:max-w-[80%] mx-auto py-16 px-4 lg:px-0 space-y-8">
+      <h2 class="text-2xl lg:text-4xl leading-snug font-medium lg:font-semibold ">SOME BOLD TEXT</h2>
+      <p class="font-light text-sm lg:text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda nulla ex quas necessitatibus dolore molestiae facilis beatae, reiciendis facere aliquam animi? Quibusdam vitae harum dolores voluptate, recusandae quidem possimus dolore.</p>
+    </div>
+  </div>
+</template>
