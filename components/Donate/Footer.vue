b24ec72f2c83ec3ad3bf1bdcd77fda8f3cb460ec
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-04-19 19:43:27 +0100

    made changes: ''

--- components/Donate/Footer.vue
+++ components/Donate/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-primary-950 text-white text-center py-8">
+  <footer class="bg-primary-950 text-white lg:text-center text-left px-4 py-8">
     <p class="py-4 lg:py-0">&copy; <span class=" lg:font-bold lg:text-lg">{{ new Date().getFullYear() }}</span> ZeroCancer. All rights reserved.</p>
   </footer>
 </template>
