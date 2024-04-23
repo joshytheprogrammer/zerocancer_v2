2b3a37f1d04bfc3308389afcdde55685985df23c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-04-23 11:29:48 +0100

    made changes

--- components/Donate/Jumbotron.vue
+++ components/Donate/Jumbotron.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="border-b h-fit bg-cover bg-center bg-no-repeat ">
-    <div class="flex justify-start align-middle items-start flex-col lg:max-w-[80%] mx-auto py-16 px-4 lg:px-0 space-y-8">
+    <div class="flex justify-start align-middle items-start flex-col lg:max-w-[80%] mx-auto py-28 px-4 lg:px-0 space-y-8">
       <h2 class="text-2xl lg:text-4xl leading-snug font-medium lg:font-semibold ">Join Us in the Fight Against Cancer in Africa!</h2>
       <p class="font-light text-sm lg:text-base">Welcome to Zero Cancer's Donate Page! Together, we're on a mission to eradicate cancer from Nigeria and beyond. As a community-driven organization, your support fuels vital initiatives aimed at reducing cancer cases across Africa. With transparent and accountable practices, your donations make a tangible impact. Explore our causes, each a crucial step towards our goal of zero cancer. We promise to utilize your donation only once a cause's goal is met, ensuring maximum impact. Join us in the fight against cancer today and make a difference together!</p>
     </div>
